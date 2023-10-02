3f24cd580d66a76b95f778cbec08137eb6763793
Sanoop Philip <99004503+Sanoop369@users.noreply.github.com>
2023-10-02 21:46:20 +0530

    Update settings.py

--- Skin_Cancer/settings.py
+++ Skin_Cancer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!4pe+ohs@kc0grb41i#6$^hl9$va7$34o(!51dt$2@lj-z7i1k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
