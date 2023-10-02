e1c1651ecac9bafe16b3fa533b32c42ea43f426d
Sanoop Philip <99004503+Sanoop369@users.noreply.github.com>
2023-10-02 23:52:04 +0530

    Update settings.py

--- Skin_Cancer/settings.py
+++ Skin_Cancer/settings.py
@@ -130,5 +130,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS=os.path.join(BASE_DIR,'static'),
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
